e36f91a3c6f677a9a2c7cebefe5eaff0c6935e25
tommy <tommy@DESKTOP-02AUUG3>
2021-09-07 12:20:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Assignment_1___Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Professor!");
+        }
+    }
+}
